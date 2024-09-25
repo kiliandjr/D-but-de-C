5a9f7dec2f1448387c8c03de9df5af9c12fe8cac
zebi31 <kilian.djouber2@gmail.com>
2024-09-25 14:42:09 +0200

    Exercice5

---
+++ ex5/ex5.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+int main()
+{
+
+	double cinq=12;
+        double quatre=7;
+        double résulta= (quatre + cinq)/2;
+        printf("(%.2f + %2.f)/2 = %2.f\n", cinq, quatre, résulta);
+        exit(0);
+}
